f017d454b80591225054572faa5afcbe9256f602
GivX <romenzonez@gmail.com>
2022-05-16 15:40:06 +0700

    implemented multiply and divide testing

--- src/createServer.js
+++ src/createServer.js
@@ -24,6 +24,24 @@ const createServer = ({ mathBasic }) => {
                 const value = mathBasic.subtract(Number(a), Number(b))
                 return { value }
             }
+        },
+        {
+            method: 'GET',
+            path: '/multiply/{a}/{b}',
+            handler: (request) => {
+                const { a, b } = request.params
+                const value = mathBasic.multiply(Number(a), Number(b))
+                return { value }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/divide/{a}/{b}',
+            handler: (request) => {
+                const { a, b } = request.params
+                const value = mathBasic.divide(Number(a), Number(b))
+                return { value }
+            }
         }
     ])
 
